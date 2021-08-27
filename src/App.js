c59263f60cde81b9131dfc37d4a9109b389d7341
Anshul Raheja <anshul.r1610@gmail.com>
2021-08-28 03:28:39 +0530

    feat: add menu functionality component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css'
 import Readmore from './components/ShowHideData/ReadMore.js';
 import Card from './components/cardCarousel/Card.js';
 import Accordion from './components/Accordion/Accordion.jsx'
+import Menu from './components/MenuFunctionality/Menu';
 function App() {
   return (
     <div className="App">
         {/* <Readmore/> */}
         {/* <Card/> */}
-        <Accordion/>
+        {/* <Accordion/> */}
+        <Menu/>
     </div>
   );
 }
---
+++ src/components/MenuFunctionality/Menu.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react'
+import styles from './menu.module.css'
+import data from './data'
+
+// category component
+const Category = ({allCategories, filterItems}) => {
+    return (
+    <section className={styles.categorySection}>
+        <div className={styles.btnContainer}>
+            {allCategories.map((category, index) =>{
+                return <button 
+                key={index}
+                className={styles.filterBtn}
+                onClick={()=> filterItems(category)}
+                >{category}</button>
+            })}
+        </div>
+    </section>
+    )
+}
+
+// menu item component
+const MenuData = ({menuItems}) => {
+    return (
+    <section className={styles.menuSection}>
+        {menuItems.map((item)=>{
+            const {id, title, img, desc, price} = item;
+            return (
+                <article key={id} className={styles.menuItem}>
+                    <div className={styles.itemInfo}>
+                        <header>
+                            <h4>{title}</h4>
+                            <h4 className={styles.Itemprice}>{price}</h4>
+                        </header>
+                        <p className={styles.itemtext}>{desc}</p>
+                    </div>
+                </article>
+            );
+        })}
+    </section>
+    );
+};
+
+// main component
+const Menu = () => {
+    const allCategories = ['all',...new Set(data.map((item)=> item.category))];
+    const [menuItems, setMenuItems] = useState(data);
+    const [categories, useCategories] = useState(allCategories);
+
+
+    const filterItems = (category) => {
+        if(category === 'all'){
+            setMenuItems(data);
+            return;
+        }
+        const newItems = data.filter((item)=>item.category === category);
+        setMenuItems(newItems);
+
+    }
+    return (
+    <div>
+        <section className={styles.mainSection}>
+            <h1>Menu functionality</h1>
+            <Category categories={categories} filterItems={filterItems}/>
+            <MenuData menuItems={menuItems}/>
+        </section>
+    </div>
+    )
+}
+
+export default Menu
